450a0841c915c533b00863b4d12cea487a5bd8be
EkaterinaArslanbaeva <ekaterina.arsla@gmail.com>
2021-03-01 00:47:38 -0500

    E-api to get latest request from db

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.post("/api/auth/login", AuthControls.login);
 app.get("/api/auth", requireLogin, AuthControls.current);
 
 app.post("/api/requests/new", RequestControls.create);
+app.get("/api/requests/latest", RequestControls.getLatest);
 app.get("/api/requests/active", RequestControls.getActive);
 app.get("/api/requests/all", RequestControls.getAll);
 app.get("/api/requests/:id", RequestControls.getRequestById);
