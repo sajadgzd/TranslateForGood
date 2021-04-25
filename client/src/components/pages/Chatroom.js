4f7229a1763be3ca52162792da52a3a558ef2d49
MarinaOrzechowski <m.skachko@hotmail.com>
2021-04-25 16:53:20 -0400

    display rooms where user is T or U separately

--- client/src/components/pages/Chatroom.js
+++ client/src/components/pages/Chatroom.js
@@ -3,29 +3,37 @@ import React, { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
 
 const Chatroom = () => {
-    // const [chatrooms, setChatrooms] = useState([]);
-    
-    // const getChatrooms = () => {
-    //     axios
-    //     .get("/api/chat/getChats", {
-    //         headers: {
-    //             Authorization : "Bearer" + localStorage.getItem("token"),
-    //         }
-    //     })
-    //     .then((response) => {
-    //         setChatrooms(response.data);
-    //     }).catch((err) => {
-    //         setTimeout(getChatrooms, 3000);
-    //     });
-    // };
 
-    // useEffect(() => {
-    //     getChatrooms();
-    // }, []);
+    // const [chatrooms, setChatroomsList] = useState([]);
+    const [chatroomsTranslator, setChatroomsTranslator] = useState([]);
+    const [chatroomsRequester, setChatroomsRequester] = useState([]);
 
-    const [chatrooms, setChatroomsList] = useState([]);
-    // retrieve all chatrooms available to current user
-    const getChatroomsList = async () => {
+
+    // chatrooms where user is a translator
+    const getChatroomsTranslator = async () => {
+
+        const res = await axios.get("/api/auth", {
+            headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`, 
+            },
+        });
+        let user = res.data;
+        let requestsList = user.translationActivity.accepted;
+        const chatroomsTranslator = await axios.get("/api/chat/filter", { 
+            params: {
+                requestsList  
+            }
+          });
+
+          setChatroomsTranslator(chatroomsTranslator.data);
+        
+    };
+    useEffect(() => {
+        getChatroomsTranslator();
+    }, []);
+
+    // chatrooms where user is a requester
+    const getChatroomsRequester = async () => {
 
         const res = await axios.get("/api/auth", {
             headers: {
@@ -34,37 +42,51 @@ const Chatroom = () => {
         });
         let user = res.data;
         // console.log(user);
-        let requestTransl = user.translationActivity.accepted;
-        let requestUser = user.requests;
-        let requestsSet = new Set(requestTransl.concat(requestUser));
-        let requestsList = [...requestsSet];
-        const chatrooms = await axios.get("/api/chat/filter", { 
+        let requestsList = user.requests;
+        const chatroomsRequester = await axios.get("/api/chat/filter", { 
             params: {
                 requestsList  
             }
           });
 
-        setChatroomsList(chatrooms.data);
+          setChatroomsRequester(chatroomsRequester.data);
         
     };
     useEffect(() => {
-        getChatroomsList();
+        getChatroomsRequester();
     }, []);
+
 
     return (
         <div>
-            <div>Chatrooms</div>
-            <div className="chatrooms">
-                {chatrooms.map((chatroom) => (
-                    <div key={chatroom._id} className="chat">
-                        <div>{chatroom.name}</div>
-                        <Link to={"/chat/" + chatroom._id}>
-                            <div>Join</div>
-                        </Link>
-                    </div>
-                ))}
+            <div>
+                <div>Translate:</div>
+                <div className="chatrooms">
+                    {chatroomsTranslator.map((chatroom) => (
+                        <div key={chatroom._id} className="chat">
+                            <div>{chatroom.name}</div>
+                            <Link to={"/chat/" + chatroom._id}>
+                                <div>Join</div>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <div>Get Translation:</div>
+                <div className="chatrooms">
+                    {chatroomsRequester.map((chatroom) => (
+                        <div key={chatroom._id} className="chat">
+                            <div>{chatroom.name}</div>
+                            <Link to={"/chat/" + chatroom._id}>
+                                <div>Join</div>
+                            </Link>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
+        
     )
 };
 
