25e466d59e1758a92339e338ff1c0b715e9d864a
MarinaOrzechowski <m.skachko@hotmail.com>
2021-05-18 13:42:12 -0400

    renders offline page

---
+++ client/public/offlineAbout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import MainView from '../src/components/MainView';
+import Footer from '../src/components/Footer';
+import Cards from '../src/components/Cards';
+
+function offlineAbout(props) {
+  return (
+    <>
+      <MainView />
+      <Cards />
+      <Footer />
+      
+    </>
+  );
+}
+
+export default offlineAbout;
--- client/public/sw.js
+++ client/public/sw.js
@@ -1,3 +1,4 @@
+//import { response } from "express";
 
 self.addEventListener('push', function(event) {
   event.waitUntil(
@@ -29,5 +30,49 @@ self.addEventListener('notificationclick', function(event) {
         return self.clients.openWindow('http://localhost:3000/chatList');
       }
     })
+  );
+});
+
+self.addEventListener('install', function(event) {
+  console.log("SW: Installing service worker...", event);
+  event.waitUntil(
+    caches.open("static")
+    .then(function(cache){
+      console.log("SW: Precaching app shell");
+      return cache.addAll(
+        [
+          "offlineAbout.js"
+        ]
+      );
+    })
+    )
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    (async () => {
+      try {
+        // First, try to use the navigation preload response if it's supported.
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        // Always try the network first.
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+        // catch is only triggered if an exception is thrown, which is likely
+        // due to a network error.
+        // If fetch() returns a valid HTTP response with a response code in
+        // the 4xx or 5xx range, the catch() will NOT be called.
+        console.log("Fetch failed; returning offline page instead.", error);
+
+        const cache = await caches.open("static");
+        const cachedResponse = await cache.match("/offlineAbout.js"); 
+        return cachedResponse;
+      }
+    })()
   );
 });
